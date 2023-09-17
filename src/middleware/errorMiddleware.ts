e59b3c9637faaedd5ab52b4167c6a0c13c8d9174
KrzysztofPogorzelski <krzysztof.pogorzelski@autoprotectgroup.co.uk>
2023-09-17 01:15:16 +0100

    [Bugfix/Auth] Fixing regiser user controller, improving errorMiddleware, adding new endpoint for auth check

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -1,4 +1,4 @@
-import ErrorResponse from '../utils/ErrorResponse.js'
+import ErrorResponse from '../utils/ErrorResponse'
 import type { Request, Response, NextFunction } from 'express'
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
@@ -8,13 +8,65 @@ const notFound = (req: Request, res: Response, next: NextFunction) => {
 }
 
 const errorHandler = (err: ErrorResponse, req: Request, res: Response, next: NextFunction) => {
-    let error = { ...err }
+    let error = { ...err };
     error.message = err.message
 
-    res.status(error.statusCode || 500).json({
-        message: error.message || 'Server Error',
-        stack: process.env.NODE_ENV !== 'production' && error.stack,
-    })
+    // Log for debugging
+    console.error(err);
+
+    switch (err.code) {
+        // Unique Constraint
+        case 'P2002':
+            if (err.meta && err.meta.target) {
+                const target = err.meta.target.join(', ');
+                error = new ErrorResponse(`${target} already exists`, 400);
+            }
+            break;
+
+        // Record Not Found
+        case 'P2001':
+            error = new ErrorResponse('The specified record does not exist', 404);
+            break;
+
+        // Foreign Key Constraint
+        case 'P2003':
+            error = new ErrorResponse('Invalid reference. The related record does not exist.', 400);
+            break;
+
+        // Mandatory Value Missing
+        case 'P2004':
+            error = new ErrorResponse('A mandatory value is missing.', 400);
+            break;
+
+        // Invalid Value Provided
+        case 'P2005':
+            error = new ErrorResponse('An invalid value was provided.', 400);
+            break;
+
+        // Connection Issues
+        case 'P2009':
+        case 'P2010':
+            error = new ErrorResponse('Database connection error. Please try again.', 503);
+            break;
+
+        // Raw Query Fail
+        case 'P2015':
+            error = new ErrorResponse('Database query failed.', 500);
+            break;
+
+        // Default for other Prisma errors or if the error isn't a Prisma error
+        default:
+            if (!error.statusCode) {
+                error.statusCode = 500;
+                error.message = 'Server Error';
+            }
+    }
+
+    res.status(error.statusCode).json({
+        success: false,
+        message: error.message,
+        stack: process.env.NODE_ENV !== 'production' ? error.stack : undefined
+    });
 }
 
 export { notFound, errorHandler }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, logoutUser } from '../controllers/auth';
+import { registerUser, loginUser, logoutUser, checkUser } from '../controllers/auth';
 import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/logout').post(authMiddleware, logoutUser)
-
+router.route('/check').post(authMiddleware, checkUser)
 
 export default router;
